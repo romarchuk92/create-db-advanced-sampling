eb8ae7b0c41cdff05a3740291bc6443774c151c1
Alexey Romarchuk <romarchuk@mail.ru>
2023-06-09 00:16:19 +0300

    HW Task №3

--- hw.sql
+++ hw.sql
@@ -122,3 +122,34 @@ select track_name from track
 where track_name like '%(my %' or track_name like 'my %' or track_name like 'my %' or track_name like '% my %' or track_name like '% my' or track_name like '% my)' or track_name like '%(мой %' or track_name like 'мой %' or track_name like 'мой %' or track_name like '% мой %' or track_name like '% мой' or track_name like '% мой)';
  
 
+--=========Задание №3======================================================
+
+
+select genre_name, count(GenerArtist.artist_id) as cnt from Genre 
+left join GenerArtist on Genre.genre_id = GenerArtist.genere_id
+group by genre_name;
+
+select album_name, count(track.album_id) as cnt from Album 
+left join Track on Album.album_id = Track.album_id
+where album_year between 2019 and 2020 
+group by album_name;
+
+select album_name, avg(track.track_time) from Album 
+left join Track on Album.album_id = Track.album_id 
+group by album_name;
+
+select artist_name, album_year from Artist 
+left join ArtistAlbum on Artist.artist_id = ArtistAlbum.artist_id
+left join Album on ArtistAlbum.album_id = Album.album_id
+where album_year != 2020
+group by artist_name, album_year;
+
+select digest_name, artist_name from Digest 
+left join TrackDigest on Digest.digest_id = TrackDigest.digest_id
+left join Track on TrackDigest.track_id = Track.track_id
+left join Album on Track.album_id = Album.album_id
+left join ArtistAlbum on Album.album_id = ArtistAlbum.album_id
+left join Artist on ArtistAlbum.artist_id = Artist.artist_id
+where artist_name = 'Баста'
+group by digest_name, artist_name;
+
